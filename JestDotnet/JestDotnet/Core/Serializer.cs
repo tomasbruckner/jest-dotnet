18ea465749f4ec9167168beda3896d712a0d5a81
Tomas Bruckner <tomasbrucknermail@gmail.com>
2023-02-19 11:48:02 +0100

    unify serializer for diff and saving snapshots

---
+++ JestDotnet/JestDotnet/Core/Serializer.cs
@@ -0,0 +1,19 @@
+﻿using JestDotnet.Core.Settings;
+
+namespace JestDotnet.Core
+{
+    internal static class Serializer
+    {
+        internal static string Serialize(object obj)
+        {
+            var jsonSerializer = SnapshotSettings.CreateJsonSerializer();
+            using var jsonWriter = SnapshotSettings.CreateJTokenWriter();
+            jsonSerializer.Serialize(jsonWriter, obj);
+            using var stringWriter = SnapshotSettings.CreateStringWriter();
+            using var jsonTextWriter = SnapshotSettings.CreateTextWriter(stringWriter);
+            jsonWriter.Token!.WriteTo(jsonTextWriter);
+            
+            return stringWriter.ToString();
+        }
+    }
+}
