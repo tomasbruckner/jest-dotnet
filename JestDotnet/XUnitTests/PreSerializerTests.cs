9fbaca939f52aa1788516df5ca48fa2e5f60c8fa
Tomas Bruckner <tomasbrucknermail@gmail.com>
2026-03-12 04:44:59 +0100

    test: add edge case tests for pre-serializer hook

--- JestDotnet/XUnitTests/PreSerializerTests.cs
+++ JestDotnet/XUnitTests/PreSerializerTests.cs
@@ -25,6 +25,78 @@ public class PreSerializerTests
             SnapshotSettings.ClearPreSerializers();
         }
     }
+
+    [Fact]
+    public void PreSerializerShouldPreserveKeyOrder()
+    {
+        SnapshotSettings.AddPreSerializer<CustomSerializable>(
+            obj => $$"""{"zName": "{{obj.Name}}", "aAge": {{obj.Age}}}""");
+        try
+        {
+            var obj = new CustomSerializable("Bob", 25);
+            JestAssert.ShouldMatchInlineSnapshot(obj, """
+                {
+                  "zName": "Bob",
+                  "aAge": 25
+                }
+                """);
+        }
+        finally
+        {
+            SnapshotSettings.ClearPreSerializers();
+        }
+    }
+
+    [Fact]
+    public void ObjectsWithoutPreSerializerUseDefaultSerialization()
+    {
+        SnapshotSettings.AddPreSerializer<CustomSerializable>(obj => obj.ToJson());
+        try
+        {
+            var person = new { FirstName = "Alice", Age = 30 };
+            JestAssert.ShouldMatchInlineSnapshot(person, """
+                {
+                  "Age": 30,
+                  "FirstName": "Alice"
+                }
+                """);
+        }
+        finally
+        {
+            SnapshotSettings.ClearPreSerializers();
+        }
+    }
+
+    [Fact]
+    public void ClearPreSerializersShouldRemoveAllRegistrations()
+    {
+        SnapshotSettings.AddPreSerializer<CustomSerializable>(obj => obj.ToJson());
+        SnapshotSettings.ClearPreSerializers();
+
+        var obj = new CustomSerializable("Alice", 30);
+        JestAssert.ShouldMatchInlineSnapshot(obj, """
+            {
+              "Age": 30,
+              "Name": "Alice"
+            }
+            """);
+    }
+
+    [Fact]
+    public void ShouldMatchObjectWorksWithPreSerializer()
+    {
+        SnapshotSettings.AddPreSerializer<CustomSerializable>(obj => obj.ToJson());
+        try
+        {
+            var a = new CustomSerializable("Alice", 30);
+            var b = new CustomSerializable("Alice", 30);
+            JestAssert.ShouldMatchObject(a, b);
+        }
+        finally
+        {
+            SnapshotSettings.ClearPreSerializers();
+        }
+    }
 }
 
 public class CustomSerializable
